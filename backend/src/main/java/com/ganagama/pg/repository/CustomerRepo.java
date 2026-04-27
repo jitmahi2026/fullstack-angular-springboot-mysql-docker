ef4b2a275a50f69cc3065a62195972b3bf9d70a2
jitmahi2026 <mahajanjitendra504@gmail.com>
2026-04-27 15:44:43 +0530

    fix repo

--- backend/src/main/java/com/ganagama/pg/repository/CustomerRepo.java
+++ backend/src/main/java/com/ganagama/pg/repository/CustomerRepo.java
@@ -1,11 +1,12 @@
 package com.ganagama.pg.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ganagama.pg.model.Customer;
 
 @Repository
-public interface CustomerRepo extends CrudRepository<Customer, Long>{
+public interface CustomerRepo extends JpaRepository<Customer, Long>{
 
 }
