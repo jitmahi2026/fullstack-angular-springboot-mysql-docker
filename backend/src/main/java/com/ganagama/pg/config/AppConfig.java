87dc233b73812cb8312349a3e2a3742fce813767
jitmahi2026 <mahajanjitendra504@gmail.com>
2026-04-30 16:20:33 +0530

    fix Appcofig

--- backend/src/main/java/com/ganagama/pg/config/AppConfig.java
+++ backend/src/main/java/com/ganagama/pg/config/AppConfig.java
@@ -6,54 +6,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-	@Value("${key}")
-	private String key;
-	@Value("${salt}")
-	private String salt;
-	@Value("${productinfo}")
-	private String productinfo;
-	@Value("${merchantId}")
-	private String merchantId;
-	@Value("${accessToken}")
-	private String accessToken;
 	@Value("${imageFilePath}")
 	private String imageFilePath;
-	public String getKey() {
-		return key;
-	}
-	public void setKey(String key) {
-		this.key = key;
-	}
-	public String getSalt() {
-		return salt;
-	}
-	public void setSalt(String salt) {
-		this.salt = salt;
-	}
-	public String getProductinfo() {
-		return productinfo;
-	}
-	public void setProductinfo(String productinfo) {
-		this.productinfo = productinfo;
-	}
-	public String getMerchantId() {
-		return merchantId;
-	}
-	public void setMerchantId(String merchantId) {
-		this.merchantId = merchantId;
-	}
-	public String getAccessToken() {
-		return accessToken;
-	}
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
+
 	public String getImageFilePath() {
 		return imageFilePath;
 	}
+
 	public void setImageFilePath(String imageFilePath) {
 		this.imageFilePath = imageFilePath;
 	}
 	
+		
 	
 }
