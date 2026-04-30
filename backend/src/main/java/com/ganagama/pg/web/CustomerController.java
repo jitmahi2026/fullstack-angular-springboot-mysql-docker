e146c6de8f86476ceff1f7919673450280e989b6
jitmahi2026 <mahajanjitendra504@gmail.com>
2026-04-30 13:23:50 +0530

    implemented multifilepard add aadhar card fild

--- backend/src/main/java/com/ganagama/pg/web/CustomerController.java
+++ backend/src/main/java/com/ganagama/pg/web/CustomerController.java
@@ -1,18 +1,25 @@
 package com.ganagama.pg.web;
 
+import java.io.File;
 import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ganagama.pg.config.AppConfig;
 import com.ganagama.pg.domain.CustomerDTO;
 import com.ganagama.pg.model.Customer;
 import com.ganagama.pg.service.CustomerService;
@@ -24,11 +31,35 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
+	
+	@Autowired
+	private AppConfig appConfig;
+	
 	//customer/v1/customerform
-	@PostMapping(path = "/v1/customerform", produces = "application/json")
-	public ResponseEntity<Customer> saveCustomerForm(@RequestBody CustomerDTO customerDto)throws Exception{
+	@PostMapping(path = "/v1/customerform", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Customer> saveCustomerForm(@RequestBody CustomerDTO customerDto, @RequestParam("aadharCard") MultipartFile aadharCard)throws Exception{
 		 
 		Customer responce = null;
+		
+		try {
+
+			if (aadharCard.isEmpty() == false) {
+				String displayFileName = aadharCard.getOriginalFilename();
+
+				String displayExtention = "";
+
+				if (displayFileName.lastIndexOf(".") != -1 && displayFileName.lastIndexOf(".") != 0)
+					displayExtention = displayFileName.substring(displayFileName.lastIndexOf(".") + 1);
+
+				displayFileName = RandomStringUtils.randomAlphanumeric(15) + "." + displayExtention;
+
+				customerDto.setAddharCard(displayExtention);
+
+				FileCopyUtils.copy(aadharCard.getBytes(), new File(appConfig.getImageFilePath(), displayFileName));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		 
 		responce = customerService.customerForm(customerDto);
 		
--- frontend/src/app/component/admission-form/admission-form.component.ts
+++ frontend/src/app/component/admission-form/admission-form.component.ts
@@ -13,12 +13,38 @@ import { Router } from '@angular/router';
 export class AdmissionFormComponent {
 
   formData: any = {};
+  selectedAadharFile: File | null = null;
 
   constructor(private router: Router,private service: AdmissionService) { }
   
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedAadharFile = file;
+    }
+  }
+
   onSubmit() {
+
+    const formDataToSend = new FormData();
+
+    formDataToSend.append('name', this.formData.name);
+    formDataToSend.append('emailID', this.formData.emailID);
+    formDataToSend.append('mobileNo', this.formData.mobileNo);
+    formDataToSend.append('gender', this.formData.gender);
+    formDataToSend.append('roomType', this.formData.roomType);
+    formDataToSend.append('joiningDate', this.formData.joiningDate);
+    formDataToSend.append('monthlyPkg', this.formData.monthlyPkg);
+    formDataToSend.append('advancePayment', this.formData.advancePayment);
+    formDataToSend.append('roomNo', this.formData.roomNo);
+    formDataToSend.append('address', this.formData.address || '');
+
+    if (this.selectedAadharFile) {
+      formDataToSend.append('aadharCard', this.selectedAadharFile);
+    }
+
     console.log('Form submitted:', this.formData);
-    this.service.saveAdmission(this.formData).subscribe({
+    this.service.saveAdmission(formDataToSend).subscribe({
       next: (response) => {
         console.log('Admission saved:', response);
         alert('Admission submitted successfully!');
