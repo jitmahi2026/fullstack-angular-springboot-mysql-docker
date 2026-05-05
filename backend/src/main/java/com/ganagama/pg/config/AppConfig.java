227701713ed01a091e659bbfc4c1727166baf942
jitmahi2026 <mahajanjitendra504@gmail.com>
2026-05-05 22:21:13 +0530

    config

--- backend/src/main/java/com/ganagama/pg/config/AppConfig.java
+++ backend/src/main/java/com/ganagama/pg/config/AppConfig.java
@@ -1,7 +1,10 @@
 package com.ganagama.pg.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -17,6 +20,18 @@ public class AppConfig {
 		this.imageFilePath = imageFilePath;
 	}
 	
-		
+	  @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**")
+	                        .allowedOrigins("https://my-frontend-wsti.onrender.com")
+	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                        .allowedHeaders("*")
+	                        .allowCredentials(true);
+	            }
+	        };
+	    }
 	
 }
