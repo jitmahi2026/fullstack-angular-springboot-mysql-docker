95d04990a3e24415f884e5e14e97a1a1ad3e5375
jitmahi2026 <mahajanjitendra504@gmail.com>
2026-05-06 15:26:56 +0530

    change URI slash

--- frontend/src/app/services/admission.service.ts
+++ frontend/src/app/services/admission.service.ts
@@ -10,7 +10,7 @@ export class AdmissionService {
 
   // private apiUrl = 'http://localhost:8081/customer/v1/customerform';
     // private apiUrl = '/api/customer/v1/customerform';
-      private apiUrl = `${environment.apiUrl}/customer/v1/customerform`;
+      private apiUrl = `${environment.apiUrl}customer/v1/customerform`;
   constructor(private http: HttpClient) { }
 
    saveAdmission(data: any): Observable<any> {
